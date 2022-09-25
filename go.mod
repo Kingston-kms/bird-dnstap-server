f4629ac38fc4340a39f5cb1040b5408154c95300
Kingston-kms <kingston1853@gmail.com>
2022-09-25 11:09:14 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kingston-kms/bird-dnstap-server
+
+go 1.15
+
+require github.com/go-chi/chi/v5 v5.0.7
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kingston-kms/bird-dnstap-server/server"
+)
+
+func main() {
+	server.RunServer();
+}
